f8e120e882ce45217cffc8cbd6f543e475c9d1ac
aomarzai <aomarzai@gmail.com>
2024-11-26 20:35:13 -0500

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Sam on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
